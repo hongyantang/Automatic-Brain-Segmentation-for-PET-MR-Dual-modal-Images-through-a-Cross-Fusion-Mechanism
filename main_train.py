59d53ea8755a558185aa8cb540d43cd4d21d6f70
hongyantang <140797763+hongyantang@users.noreply.github.com>
2024-07-15 17:44:17 +0800

    main_train.py

--- main_train.py
+++ main_train.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul  3 11:06:19 2021
-
-@author: leeh43
-"""
 
 from monai.utils import set_determinism
 from monai.transforms import AsDiscrete
